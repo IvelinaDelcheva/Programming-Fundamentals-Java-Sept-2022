c2c10636a0fe3e6de8cf69ca06d7dc8d4ca0a87e
Ivelina Delcheva <110343307+IvelinaDelcheva@users.noreply.github.com>
2023-01-08 11:07:31 +0000

    Problems Solutions

---
+++ Data Types and Variables - Lab/LowerOrUpper_08.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class LowerOrUpper_08 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char letter = scanner.nextLine().charAt(0);
+
+//        if(letter >= 'a' && letter <= 'z'){
+//            System.out.println("lower-case");
+//        }else{
+//            System.out.println("upper-case");
+//        }
+
+        if(Character.isLowerCase(letter)){
+            System.out.println("lower-case");
+        }else if(Character.isUpperCase(letter)){
+            System.out.println("upper-case");
+        }
+    }
+}
